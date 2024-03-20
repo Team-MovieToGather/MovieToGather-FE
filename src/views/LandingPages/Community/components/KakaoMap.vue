39a9261eb9bdb8aa242260737aa84cf7b2cf4366
재영이 <jeayeong70@gmail.com>
2024-03-20 10:29:31 +0900

    Feat : "카카오맵 추가"

--- src/views/LandingPages/Community/components/KakaoMap.vue
+++ src/views/LandingPages/Community/components/KakaoMap.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
     <div id="map"></div>
-    <div class="button-group">
-      <button @click="changeSize(0)">Hide</button>
-      <button @click="changeSize(400)">show</button>
-      <button @click="displayMarker(markerPositions1)">marker set 1</button>
-      <button @click="displayMarker(markerPositions2)">marker set 2</button>
-      <button @click="displayMarker([])">marker set 3 (empty)</button>
-      <button @click="displayInfoWindow">infowindow</button>
-    </div>
   </div>
 </template>
 
@@ -18,21 +10,6 @@ export default {
   name: "KakaoMap",
   data() {
     return {
-      markerPositions1: [
-        [33.452278, 126.567803],
-        [33.452671, 126.574792],
-        [33.451744, 126.572441],
-      ],
-      markerPositions2: [
-        [37.499590490909185, 127.0263723554437],
-        [37.499427948430814, 127.02794423197847],
-        [37.498553760499505, 127.02882598822454],
-        [37.497625593121384, 127.02935713582038],
-        [37.49629291770947, 127.02587362608637],
-        [37.49754540521486, 127.02546694890695],
-        [37.49646391248451, 127.02675574250912],
-      ],
-      markers: [],
       infowindow: null,
     };
   },
@@ -44,7 +21,7 @@ export default {
       /* global kakao */
       script.onload = () => kakao.maps.load(this.initMap);
       script.src =
-        "//dapi.kakao.com/v2/maps/sdk.js?appkey=1977c706ad34cbefe247fce2abc86814";
+        "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=1977c706ad34cbefe247fce2abc86814";
       document.head.appendChild(script);
     }
   },
